57107136b6dd00b4d9e2f5fef085dbcdf1c9f0a5
Bas Driessen <bas_driessen@hotmail.com>
2020-04-09 21:32:52 +0200

    add settings test

---
+++ src/PackageSample.UI/IntroWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PackageSample.UI
+{
+    /// <summary>
+    /// Interaction logic for IntroWindow.xaml
+    /// </summary>
+    public partial class IntroWindow : Window
+    {
+        public IntroWindow()
+        {
+            InitializeComponent();
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+            txtSetting2.Text = localSettings.Values.ContainsKey("setting2") ? localSettings.Values["setting2"].ToString() : "";
+
+
+            if (!localSettings.Values.ContainsKey("setting1"))
+            {
+                localSettings.Values["setting1"] = 0;
+            }
+            else
+            {
+                btnAction1.Content = $"{localSettings.Values["setting1"]}++";
+            }
+
+
+            if (!localSettings.Values.ContainsKey("setting2"))
+            {
+                localSettings.Values["setting2"] = "";
+            }
+        }
+
+        private void Action1Click(object sender, RoutedEventArgs e)
+        {
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+            if (localSettings.Values.ContainsKey("setting1"))
+            {
+                var value = int.Parse(localSettings.Values["setting1"].ToString());
+                value++;
+                localSettings.Values["setting1"] = value;
+            }
+            else
+            {
+                localSettings.Values["setting1"] = 1;
+            }
+
+            
+        }
+
+        private void Action2Click(object sender, RoutedEventArgs e)
+        {
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            localSettings.Values.Add("intro", 1);
+            this.Close();
+        }
+
+        private void TextBoxBase_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+            localSettings.Values["setting2"] = (sender as TextBox).Text;
+        }
+    }
+}
--- src/PackageSample.UI/MainWindow.xaml.cs
+++ src/PackageSample.UI/MainWindow.xaml.cs
@@ -14,7 +14,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 using Windows.ApplicationModel;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
 using PackageSample.Library;
 
 namespace PackageSample.UI
@@ -42,6 +45,30 @@ namespace PackageSample.UI
                 txtArgs.Text = string.Join(Environment.NewLine, args);
             else
                 txtArgs.Text = "N/A";
+
+
+            try
+            {
+                var test = Package.Current;
+            }
+            catch (InvalidOperationException e)
+            {
+
+            }
+
+
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+            if (!localSettings.Values.ContainsKey("intro"))
+            {
+                var intro = new IntroWindow();
+                intro.ShowDialog();
+            }
+            
+
+            txtSetting1.Text = localSettings.Values.ContainsKey("setting1") ? localSettings.Values["setting1"].ToString() : "";
+            txtSetting2.Text = localSettings.Values.ContainsKey("setting2") ? localSettings.Values["setting2"].ToString() : "";
+
         }
 
         private void BrowsePackageClick(object sender, RoutedEventArgs e)
@@ -75,12 +102,149 @@ namespace PackageSample.UI
                 var result = await LibraryHelper.Check();
 
                 if (result.Availability == PackageUpdateAvailability.Available)
-                    MessageBox.Show("Restart the application to apply the updates.", "Update Available!");
+                {
+                    var loc = Package.Current.InstalledLocation;
+
+                    Toast("Restart the application to apply the updates.", "Update Available!");
+                }
+                else
+                {
+                    var loc = Package.Current.InstalledLocation;
+                    //var package = Package.Current;
+                    var packageStatus = Package.Current.Status;
+
+                    var effLoc = Package.Current.EffectiveLocation.Path;
+
+
+                    Toast("No updates available.");
+                }
             }
             catch (Exception exception)
             {
                 MessageBox.Show(exception.Message);
             }
+        }
+        private void Toast(string message, string title = "Aurora")
+        {
+            try
+            {
+                string timeString = $"{DateTime.Now:HH:mm:ss}";
+                string image = Package.Current.Logo.ToString();//"https://avatars3.githubusercontent.com/u/34661065?s=200&v=4";
+
+
+                //<image src='{image}'/>
+                string toastXmlString =
+                    $@"<toast><visual>
+            <binding template='ToastGeneric'>
+            <text>{title}</text>
+            <text>{message}</text>
+            
+            </binding>
+        </visual></toast>";
+
+
+                var xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(toastXmlString);
+
+                var toastNotification = new ToastNotification(xmlDoc);
+
+                var toastNotifier = ToastNotificationManager.CreateToastNotifier();
+                toastNotifier.Show(toastNotification);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void SampleToastClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var title = "Unearthed Arcana: Subclasses, Part 3";
+                var message = "Three new subclasses for you to playtest: the Armorer for the artificer, the Circle of the Stars for the druid, and the Fey Wanderer for the ranger.";
+                var message2 = "Aurora";
+                var productUrl = "https://aurorabuilder.com/wp-content/uploads/2019/07/unearthed-arcana-banner-1024x267.jpg";
+
+                string timeString = $"{DateTime.Now:HH:mm:ss}";
+                string image = productUrl; //Package.Current.Logo.ToString();//"https://avatars3.githubusercontent.com/u/34661065?s=200&v=4";
+
+
+                
+
+                string toastXmlString =
+                    $@"<toast launch='action=toastAction&amp;id=1&amp;id2=2'><visual>
+                               <binding template='ToastGeneric'>
+                            <text>{title}</text>
+                            <text>{message}</text>
+                            <text placement='attribution'>{message2}</text>
+                            <image src='{productUrl}' placement='hero'/>                        
+                        </binding>
+                       </visual></toast>";
+
+                var xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(toastXmlString);
+
+                var toastNotification = new ToastNotification(xmlDoc);
+
+                var toastNotifier = ToastNotificationManager.CreateToastNotifier();
+                toastNotifier.Show(toastNotification);
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        public static XmlDocument CreateToast()
+        {
+            var xDoc = new XDocument(
+                new XElement("toast",
+                    new XElement("visual",
+                        new XElement("binding", new XAttribute("template", "ToastGeneric"),
+                            new XElement("text", "C# Corner"),
+                            new XElement("text", "Do you got MVP award?")
+                        )
+                    ),// actions    
+                    new XElement("actions",
+                        new XElement("action", new XAttribute("activationType", "background"),
+                            new XAttribute("content", "Yes"), new XAttribute("arguments", "yes")),
+                        new XElement("action", new XAttribute("activationType", "background"),
+                            new XAttribute("content", "No"), new XAttribute("arguments", "no"))
+                    )
+                )
+            );
+
+            var xmlDoc = new Windows.Data.Xml.Dom.XmlDocument();
+            xmlDoc.LoadXml(xDoc.ToString());
+            return xmlDoc;
+        }
+
+        private void Action1Click(object sender, RoutedEventArgs e)
+        {
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+            localSettings.Values["setting1"] = null;
+
+            txtSetting1.Text = "";
+        }
+        private void Action2Click(object sender, RoutedEventArgs e)
+        {
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            localSettings.Values["setting2"] = null;
+
+
+            
+            txtSetting2.Text = "";
+        }
+
+        private void ClearSettingsClick(object sender, RoutedEventArgs e)
+        {
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+            localSettings.Values.Clear();
+
         }
     }
 }
